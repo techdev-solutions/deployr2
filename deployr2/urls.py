b0f078cf3754d77b5aebd049b5c28e903a108b50
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-01-31 18:02:40 +0100

    Added access to teamcity and read all branches for frontend and backend build

--- deployr2/urls.py
+++ deployr2/urls.py
@@ -4,5 +4,6 @@ from deployr2 import views
 
 urlpatterns = patterns('',
    url(r'^$', views.index, name='index'),
-   url(r'^start_deployment/$', views.start_deployment, name='start_deployment')
+   url(r'^start_deployment/$', views.start_deployment, name='start_deployment'),
+   url(r'^get_builds$', views.get_builds_for_build_type_and_branch),
 )
--- deployr2/views.py
+++ deployr2/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests
 
 # Create your views here.
+
+teamcity_auth = ('deployr2', 'deployr2')
+teamcity_rest_base = 'http://192.168.188.20:8111/app/rest'
+accept_json_header = {'Accept': 'application/json'}
 
 
 def index(request):
@@ -8,4 +15,15 @@ def index(request):
 
 
 def start_deployment(request):
-    return render(request, 'deployr2/start_deployment.html', {})
+    backend_branches = requests.get('%s/buildTypes/id:Techdev_TrackrBackend/branches' % teamcity_rest_base, auth=teamcity_auth, headers=accept_json_header)
+    frontend_branches = requests.get('%s/buildTypes/id:Techdev_TrackrFrontend/branches' % teamcity_rest_base, auth=teamcity_auth, headers=accept_json_header)
+
+    return render(request, 'deployr2/start_deployment.html', {'backend_branches': backend_branches.json()['branch'], 'frontend_branches': frontend_branches.json()['branch']})
+
+
+def get_builds_for_build_type_and_branch(request):
+    # TODO required parameters!
+    branch = request.GET.get('branch')
+    build_id = request.GET.get('build_id')
+    builds = requests.get(u'{0:s}/builds?locator=buildType:{1:s},branch:{2:s}'.format(teamcity_rest_base, build_id, branch), auth=teamcity_auth, headers=accept_json_header)
+    return HttpResponse(builds, content_type="application/json")
