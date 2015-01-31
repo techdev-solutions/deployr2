99065da705ed44da388c4c9810ffc5c67d670586
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-01-31 21:08:44 +0100

    CSS and JS into files

---
+++ deployr2/static/deployr2/js/start_deployment.js
@@ -0,0 +1,36 @@
+function loadBuilds(buildId, branch) {
+    return $.get('/get_builds', {branch: branch, build_id: buildId});
+}
+
+var viewModel = {
+    frontendBuilds: ko.observableArray(),
+    backendBuilds: ko.observableArray()
+};
+
+$(function() {
+    var $radioInputs = $('input[type="radio"]');
+    var $selects = $('select');
+
+    $radioInputs.change(function(ev) {
+        $radioInputs.attr('disabled', 'disabled');
+        $selects.attr('disabled', 'disabled');
+
+        var buildType = ev.target.name === 'frontend-branch' ? 'Techdev_TrackrFrontend' : 'Techdev_TrackrBackend';
+
+        function buildsLoaded(buildResponse) {
+            // replace build array
+            if(ev.target.name === 'frontend-branch') {
+                viewModel.frontendBuilds(buildResponse.build);
+            } else {
+                viewModel.backendBuilds(buildResponse.build);
+            }
+
+            $radioInputs.removeAttr('disabled');
+            $selects.removeAttr('disabled');
+        }
+
+        loadBuilds(buildType, ev.target.value).done(buildsLoaded);
+    });
+
+    ko.applyBindings(viewModel);
+});
