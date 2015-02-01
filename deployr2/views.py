b95d418ad960aa5db2518f393519d655e99ecabc
Moritz Schulze <moritz@frontierpsychiatrist.de>
2015-02-01 19:07:07 +0100

    Extremely hacky running version

--- deployr2/views.py
+++ deployr2/views.py
@@ -44,8 +44,12 @@ def start_deployment_post(request):
     file_preparer = FilePreparer(deployment_name)
     file_preparer.create_directory_structure()
 
+    backend_artifact = ''
     # download artifacts from teamcity into folder
     for artifact in teamcity.get_artifacts(backend_build_id):
+        # TODO hack, the backend artifact name should not be from here!
+        if artifact.name.endswith('jar'):
+            backend_artifact = artifact.name
         res = teamcity.stream_artifact(artifact.content_link)
         file_preparer.download_backend_artifact(res.raw, artifact.name)
 
@@ -53,14 +57,37 @@ def start_deployment_post(request):
         res = teamcity.stream_artifact(artifact.content_link)
         file_preparer.download_frontend_artifact(res.raw, artifact.name)
 
-    # prepare files, like nginx conf and index.html <base tag>
+    # prepare files index.html <base tag>
     # create docker container
-    # run docker container and mount the created directories
+    from docker import Client
+    c = Client('unix://var/run/docker.sock')
+
+    backend_container = c.create_container(image='java:openjdk-8', command='java -jar ' + backend_artifact, detach=True, name='backend_' + deployment_name, volumes=['/opt/trackr-backend'],
+                                           working_dir='/opt/trackr-backend')
+    c.start(backend_container['Id'], binds={'/opt/techdev/deployr2/deployments/%s/backend' % deployment_name: {'bind': '/opt/trackr-backend', 'ro': True}})
+
+    frontend_container = c.create_container(image='nginx', ports=[80], name='frontend_' + deployment_name, volumes=['/etc/nginx/conf.d/default.conf', '/usr/share/nginx/html'])
+    c.start(frontend_container['Id'], links=[('backend_' + deployment_name, 'backend')], publish_all_ports=True, binds={
+        '/opt/techdev/deployr2/default.conf': {'bind': '/etc/nginx/conf.d/default.conf', 'ro': True},
+        '/opt/techdev/deployr2/deployments/%s/frontend/www' % deployment_name: {'bind': '/usr/share/nginx/html', 'ro': True}
+    })
+
+    inspect = c.inspect_container('frontend_' + deployment_name)
+    frontend_port = inspect['NetworkSettings']['Ports']['80/tcp'][0]['HostPort'] # Help!!!
 
     # create an entity so we know of this deployment later
     from django.utils import timezone
     deployment = Deployment(id=deployment_name, create_date=timezone.now())
     deployment.save()
+
+    import augeas
+    a = augeas.Augeas(root='/')
+    a.defvar('host', '/files/etc/apache2/sites-available/deployr.conf')
+    a.defnode('newloc', '$host/VirtualHost/Location[last() + 1]', '')
+    a.set('$newloc/arg', '/' + deployment_name)
+    a.set('$newloc/directive', 'ProxyPass')
+    a.set('$newloc/*[self::directive="ProxyPass"]/arg[1]', 'http://localhost:' + frontend_port)
+    a.save()
 
     return HttpResponseRedirect(reverse('deployr2:deployment', kwargs={'deployment_name': deployment_name}))
 
